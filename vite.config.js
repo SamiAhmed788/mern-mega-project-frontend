fd134489639b41fae1cb02856822adf140f5ca93
SamiAhmed788 <sami76748@gmail.com>
2025-02-13 19:46:47 +0500

    ksk

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Default 500 ہوتا ہے، اسے 1000 یا زیادہ کر سکتے ہیں
+  },
+});
